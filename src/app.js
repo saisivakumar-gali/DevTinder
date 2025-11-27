35db37de48f62def7d2d27643259ec07091279ca
saisivakumar-gali <galisaisivakumar@gmail.com>
2025-11-27 22:40:00 +0530

    exploring GET,POST,DELETE

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 const express=require("express");
 const app=express();
 
+app.get("/user",(req,res)=>{
+    res.send({firstname:"sai",lastname:"ssk"})
+})
+
+app.post("/user",(req,res)=>{
+    res.send("data successfully saved to the database")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("data is deleted");
+})
+
 app.use("/test",(req,res)=>{
    res.send("hello from test");
 })
-app.use("/hello",(req,res)=>{
-    res.send("hello from hello");
-})
-// app.use("/",(req,res)=>{
-//     res.send("hello from home");
-// })
+
 app.listen("7777",()=>{
     console.log("server is successfully listening on port 7777");
 })
