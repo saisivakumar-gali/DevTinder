7a9b24e88a1cb68d0cee5cfc454e8c8652ebf193
saisivakumar-gali <galisaisivakumar@gmail.com>
2025-12-05 11:28:46 +0530

    Schema validations

--- src/app.js
+++ src/app.js
@@ -44,7 +44,32 @@ app.get("/feed",async (req,res)=>{
     }
 });
 
+app.delete("/user",async (req,res)=>{
+    const userId=req.body.userId;
+    try{
+        const user=await User.findByIdAndDelete(userId);
+        res.send("user deleted successfully");
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+    }
+});
 
+
+app.patch("/user",async (req,res)=>{
+    const userId=req.body.userId;
+    const data=req.body;
+    try{
+        const user=await User.findByIdAndUpdate(userId,data,{
+            runValidators:true,
+        });
+        res.send("user updated successfully");
+    }
+    catch(err){
+        res.status(400).send("something went wrong"); 
+    }
+    
+});
 
 
 connectDB().then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,56 @@
 const mongoose=require("mongoose");
 const userSchema=new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:50
     },
     lastName:{
-        type:String
+        type:String,
+         minLength:3,
+        maxLength:50
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        
     },
     password:{
-        type:String
+        type:String,
+        required:true,
     },
     age:{
-        type:Number
+        type:Number,
+        min:18
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new error("Gender data is not valid");
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://imgs.search.brave.com/pBrctCO6SvjGyV8ZowZmmDovuKag1Nfgr6rgfepyL-s/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzZlLzU5/Lzk1LzZlNTk5NTAx/MjUyYzIzYmNmMDI2/NTg2MTdiMjljODk0/LmpwZw"
+    },
+
+    about:{
+        type:String,
+        default:"This is about the user!"
+    },
+    skills:{
+        type:[String],
+
     }
-})
+},{
+    timestamps:true
+});
 
 const userModel=mongoose.model("User",userSchema);
 module.exports=userModel;
