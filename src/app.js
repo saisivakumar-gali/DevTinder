aa1a9e2b9dec52ab9660bb3b7b88065f057dd966
saisivakumar-gali <galisaisivakumar@gmail.com>
2025-12-27 17:43:33 +0530

    ..

--- src/app.js
+++ src/app.js
@@ -27,14 +27,15 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
-const cronRouter = require("./routes/cron");
 
 
-app.use("/", cronRouter);
+
+
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
-app.use("/", userRouter);
+app.use("/", userRouter);  
+
 
 
 connectDB()
